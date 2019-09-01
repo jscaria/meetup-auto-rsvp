e1b84f827277a02e70f4a8300344ac74d9d127d5
Joel Scaria <jscaria@apple.com>
2019-09-01 15:52:03 -0400

    api key is not longer supported

--- lambda_function.py
+++ lambda_function.py
@@ -10,23 +10,51 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 ses = boto3.client('ses')
 
+
 def lambda_handler(event, context):
-    # Some variables
-    meetup_event = os.environ['MEETUP_EVENT']
-    meetup_key = os.environ['MEETUP_KEY']
-    rsvp = "YES"
-    email_to = os.environ['EMAIL_TO']
-    email_from = os.environ['EMAIL_FROM']
-    tz = pytz.timezone("US/Pacific")
-
-
-    events_url = "https://api.meetup.com/{0!s}/events?sign=true&photo-host=public&page=10".format(meetup_event)
+    events_url = "https://api.meetup.com/{0!s}/events?sign=true&photo-host=public&page=10".format(os.environ['GROUP_NAME'])
     rsvp_url = "https://api.meetup.com/2/rsvp/"
     
-    events_request = requests.get(events_url)
+    headers = {
+        "Authorization": "Bearer {0!s}".format(os.environ["OAUTH_ACCESS_TOKEN"])
+    }
+    
+    events_request = requests.get(events_url, headers=headers)
     events_response = events_request.json()
     
     logger.debug("events_response: " + json.dumps(events_response, indent=4))
+    '''
+    [
+      {
+        "local_time": "11:00",
+        "local_date": "2018-09-09",
+        "link": "https://www.meetup.com/<group_name>/events/<event_id>/",
+        "visibility": "public_limited",
+        "group": {
+          "created": 1373082291000,
+          "name": "<group_nane>",
+          "id": 0,
+          "join_mode": "approval",
+          "lat": 37.31,
+          "lon": -122,
+          "urlname": "<group_url>",
+          "who": "<people>",
+          "localized_location": "<location>",
+          "region": "en_US",
+          "timezone": "US/Pacific"
+        },
+        "waitlist_count": 0,
+        "yes_rsvp_count": 23,
+        "duration": 7200000,
+        "time": 1536516000000,
+        "utc_offset": -25200000,
+        "name": "<name>",
+        "id": "<id>"
+      }
+    ]
+    '''
+    
+    rsvp = "YES"
     responses = []
     successful_rsvp = False
 
@@ -36,11 +64,10 @@ def lambda_handler(event, context):
     
       data = {
           "event_id": event_id,
-          "key": meetup_key,
           "rsvp": rsvp
       }
-    
-      rsvp_request = requests.post(rsvp_url, data=data)
+
+      rsvp_request = requests.post(rsvp_url, headers=headers, data=data)
       rsvp_response = rsvp_request.json()
       local_response = {
         "statusCode": rsvp_request.status_code,
@@ -55,19 +82,20 @@ def lambda_handler(event, context):
         
         if rsvp_request.status_code == 201:
           ts = int(rsvp_response["event"]["time"]/1000)
+          tz = pytz.timezone("US/Pacific")
           event_time = datetime.datetime.utcfromtimestamp(ts).replace(tzinfo=pytz.utc) # in UTC
           event_time_in_local = event_time.astimezone(tz) # converted to US/Pacific
           event_time_string = event_time_in_local.strftime('%m-%d-%Y @ %H:%M')
           subject = 'RSVP {0!s} to {1!s} on {2!s}'.format(rsvp_response["response"], rsvp_response["event"]["name"], event_time_string)
         
         response = ses.send_email(
-          Source = email_from,
-          Destination = {
+          Source = os.environ['EMAIL_FROM'],
+          Destination={
             'ToAddresses': [
-              email_to,
+              os.environ['EMAIL_TO'],
             ],
           },
-          Message = {
+          Message={
             'Subject': {
               'Data': subject
             },
